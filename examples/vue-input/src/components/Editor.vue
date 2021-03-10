d594af72c874c234fa4b9b7b6c2339c159e8734c
Rahul Kadyan <rahul.kadyan@grammarly.com>
2021-03-10 14:16:39 -0800

    chore: add vue input example

---
+++ examples/vue-input/src/components/Editor.vue
@@ -0,0 +1,13 @@
+<script>
+import { GrammarlyInput } from "@grammarly/vue-textarea";
+import { demoText, demoClientId } from "./demo";
+
+export default {
+  data: () => ({ text: demoText, clientId: demoClientId }),
+  components: { GrammarlyInput },
+};
+</script>
+
+<template>
+  <GrammarlyInput v-model="text" style="width: 100%" :clientId="clientId" />
+</template>
