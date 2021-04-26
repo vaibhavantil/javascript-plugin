6c3d8fa24d4457135c2c4942bb941d978089a7ec
michael.chernetsov <michael.chernetsov@grammarly.com>
2021-04-26 15:31:54 -0500

    example for editor-sdk-vue

---
+++ examples/editor-sdk-vue/src/components/Editors.vue
@@ -0,0 +1,30 @@
+<script>
+import {Grammarly, GrammarlyEditorPlugin} from "@grammarly/editor-sdk-vue";
+import {demoText, demoClientId} from "./demo";
+
+export default {
+  data: () => ({text: demoText, clientId: demoClientId}),
+  components: {Grammarly, GrammarlyEditorPlugin},
+};
+</script>
+
+<template>
+  <Grammarly clientId={demoClientId}>
+    <div>
+      <h2>Textarea</h2>
+      <GrammarlyEditorPlugin>
+        <textarea v-model="text.textarea" rows={10}></textarea>
+      </GrammarlyEditorPlugin>
+
+      <h2>Input</h2>
+      <GrammarlyEditorPlugin>
+        <input v-model="text.input" id="test-input" size="50"/>
+      </GrammarlyEditorPlugin>
+
+      <h2>Contenteditable</h2>
+      <GrammarlyEditorPlugin>
+        <div contentEditable="true" v-html="text.contenteditable"></div>
+      </GrammarlyEditorPlugin>
+    </div>
+  </Grammarly>
+</template>
